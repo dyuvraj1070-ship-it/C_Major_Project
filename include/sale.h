6d3b9c4f535884a3154d0a6b3a927a62a8f55a83
Yuvraj Dhiman <dyuvraj1070@gmail.com>
2025-11-30 14:33:32 +0000

    update header file for sale.h

---
+++ include/sale.h
@@ -0,0 +1,8 @@
+#ifndef SALE_H
+#define SALE_H
+#include "structs.h"  // Shared Sale struct
+void make_sale(void);
+void view_sales_today(void);
+void totals_today(void);
+void cancel_sale(void);
+#endif
