8d5b1807a7067960bc116fc60d71da6b4b96c738
Yuvraj Dhiman <dyuvraj1070@gmail.com>
2025-11-30 16:47:03 +0000

    sales.h changes

--- include/sales.h
+++ include/sales.h
@@ -1,39 +1,27 @@
 #ifndef SALES_H
 #define SALES_H
 
-#include <stdio.h>
-
-#define SALES_F  "sales.txt"
-#define NAME_LEN 40
-#define REG_LEN  20
-#define LINE_MAX 256
-
-typedef union {
-    char phone[16];
-    char licence[24];
-} Extra;
+#define SALES_F "sales.csv"
+#define LINE_MAX 200
+#define NAME_LEN 30
+#define REG_LEN 20
 
 typedef struct {
     int id;
     char date[11];
     char vehReg[REG_LEN];
     char driver[NAME_LEN];
-    char fuel[16];
+    char fuel[10];
     double liters;
     double rate;
     double total;
-    int prefType;
-    Extra ex;
-    char payMethod[12];
 } Sale;
 
-/* function declarations */
-char *today_date();
+char* today_date();
 int next_sale_id();
 void save_sale(Sale *s);
 void make_sale();
 void view_sales_today();
 void totals_today();
-void cancel_sale();
 
 #endif
